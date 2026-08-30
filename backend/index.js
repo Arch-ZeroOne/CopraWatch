2fffac3edced3d42160a0e824c2a3fa96e7fefef
Arch-ZeroOne <windyl1.monton.lvhs@gmail.com>
2026-08-30 15:49:49 +0800

    Finished web scraping from backend and render data to frotnend

--- backend/index.js
+++ backend/index.js
@@ -1,39 +1,54 @@
 import puppeteer from "puppeteer";
+import express from "express";
+import cors from "cors";
+const app = express();
+app.use(cors());
+app.use(express.json());
 
+const port = 3000;
+
+app.get("/", async (req, res) => {
+  const prices = await getPrices();
+
+  res.send(prices);
+});
+
+app.listen(port, () => {
+  console.log("Port is running in port:", port);
+});
 //Starting a new puppeteer session
-
-const getQuotes = async () => {
+const getPrices = async () => {
   // This code right here tells the pupeeter package to open a web browser
 
   // Params Breakdown:
   // headless -> normally this puppeteer extension doesnt show the browser opening by default  but since we enable headless mode the process of opening a browser is shown
   // defaultViewport -> uses the original size of browser
   const browser = await puppeteer.launch({
-    headless: false,
+    headless: true,
     defaultViewport: null,
   });
 
   //Opens a new page
   const page = await browser.newPage();
 
-  await page.goto("http://quotes.toscrape.com/", {
-    waitUntil: "domcontentloaded",
-  });
+  await page.goto(
+    "https://www.selinawamucii.com/insights/prices/philippines/copra/",
+    {
+      waitUntil: "domcontentloaded",
+    },
+  );
 
-  const quotes = await page.evaluate(() => {
+  const prices = await page.evaluate(() => {
     //Gets the parent element of our target component containing our target elements with our needed data in this case the author and the quote (text)
-    const quote = document.querySelector(".quote");
+    const quote = document.querySelector(".sw-rd-hl-main");
 
-    const text = quote.querySelector(".text").innerText;
-    const author = quote.querySelector(".author").innerText;
+    // const text = quote.querySelector(".text").innerText;
+    // const author = quote.querySelector(".author").innerText;
 
-    return { text, author };
+    const us = quote.querySelector(".sw-rd-hl-price").innerText;
+    const ph = quote.querySelector(".sw-rd-hl-local").innerText;
+    return { us, ph };
   });
-
-  console.log(quotes);
-
-  //Closes the browser
-  //   await browser.close();
+  await browser.close();
+  return prices;
 };
-
-getQuotes();
